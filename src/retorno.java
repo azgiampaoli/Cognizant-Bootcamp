9587b2bff6b5c33c58122c273796060aaf5d0adb
azgiampaoli <41347578+azgiampaoli@users.noreply.github.com>
2022-01-10 20:29:05 -0300

    Add files via upload

---
+++ src/retorno.java
@@ -0,0 +1,35 @@
+
+public class retorno {
+
+	
+	 public static int area(int lado) {
+		 int area = lado * lado;
+		 return area;
+	 }
+	 
+	 	 
+	 public static int area(int lado, int altura) {
+		 int area = lado * altura;
+		 return area;
+		 
+	 }
+	 
+	 public static int area(int baseMaior, int baseMenor, int altura) {
+		 
+		 int area = ((baseMenor + baseMaior) * altura)/2;
+		 return area;
+		 
+	 }
+		
+	
+
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("A area do quadrado é: " + retorno.area(2));
+		System.out.println("A area do retangulo é: " + retorno.area(3, 4));
+		System.out.println("A area do trapézio é: " + retorno.area(1, 2, 3));
+
+}
+}
