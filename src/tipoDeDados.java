7cb4da7fce2facf85ef693f137ab0f3a1a91811e
azgiampaoli <41347578+azgiampaoli@users.noreply.github.com>
2022-01-10 14:03:44 -0300

    Initial commit

---
+++ src/tipoDeDados.java
@@ -0,0 +1,42 @@
+
+public class tipoDeDados {
+
+	public static void main(String[] args) {
+		
+		byte b =15;
+		byte b1 = 32;
+		short s = -15785;
+		short s1 = 1254;
+		int i = 8515785;
+		int i1 = 6521354;
+		double d = 3.14546524d;
+		double d1 = 2.659841d;
+		float f1 = 9.56412f;
+		float f = 3.15646454f;
+		char c ='\u0054';
+		char e = 'T';
+		String r = "Angela";
+		String r1 = "Giampaoli";
+		boolean a = false;
+		boolean a1 = true;
+		
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(c);
+		System.out.println(e);
+		System.out.println(r);
+		System.out.println(a);
+		System.out.println(b1);
+		System.out.println(s1);
+		System.out.println(i1);
+		System.out.println(d1);
+		System.out.println(f1);
+		System.out.println(r1);
+		System.out.println(a1);
+		
+	}
+
+}
---
+++ src/variaveis.java
@@ -0,0 +1,38 @@
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		
+		int i;
+		int I;
+		// int 1a;
+		int _1a;
+		int $aq;
+		
+		i = 5;
+		I = 10;
+		_1a = 20;
+		$aq = 7;
+		
+		System.out.println(i);
+		System.out.println(I);
+		System.out.println(_1a);
+		System.out.println($aq);
+		
+		int quantidadeProduto = 50;
+		final int NUMERO_TENTATIVA = 5;
+		final int numeroTentativas = 5;
+		int QUANTIDADE_OPCOES = 5;
+		int qtdProd;
+		qtdProd = 12;
+		
+		System.out.println(quantidadeProduto);
+		System.out.println(NUMERO_TENTATIVA);
+		System.out.println(QUANTIDADE_OPCOES);
+		System.out.println(numeroTentativas);
+		System.out.println(qtdProd);
+		
+		
+	}
+
+}
