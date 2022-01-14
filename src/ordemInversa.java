5e1167cfe4678edb1fd0dceb9fe8cdb79fdda6bd
azgiampaoli <41347578+azgiampaoli@users.noreply.github.com>
2022-01-14 11:44:08 -0300

    Add files via upload

---
+++ src/ordemInversa.java
@@ -0,0 +1,21 @@
+
+public class ordemInversa {
+
+	public static void main(String[] args) {
+		 
+		 int[] numero = {1,2,3,4,5,6};
+		 
+		
+		 
+		 for (int i= (numero.length - 1); i >= 0; i--) {
+		 
+			 System.out.println(numero[i]);
+			
+		 
+		 }
+		
+		
+		
+	}
+
+}
