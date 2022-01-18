dbf3f00d7dcb8c5afc254ed303f14eb2a67c0cfb
azgiampaoli <41347578+azgiampaoli@users.noreply.github.com>
2022-01-18 14:34:54 -0300

    Add files via upload

---
+++ pilha/src/one/digitalinnovation/main.java
@@ -0,0 +1,33 @@
+package one.digitalinnovation;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		pilha minhaPilha = new pilha();
+		
+		minhaPilha.push(new No(1) );
+		minhaPilha.push(new No(2) );
+		minhaPilha.push(new No(3) );
+		minhaPilha.push(new No(4) );
+		minhaPilha.push(new No(5) );
+		minhaPilha.push(new No(6) );
+		
+		
+	System.out.println(minhaPilha);	
+	
+			
+	System.out.println(minhaPilha.pop());
+	
+	System.out.println(minhaPilha);
+	
+	
+	minhaPilha.push(new No(99));
+	System.out.println(minhaPilha);
+	
+	
+	
+	}
+
+}
